17c82e5516c2e7a85547132e335b87f5525203f5
bharat.mane <bharat.mane@outlook.com>
2021-10-27 16:21:42 +0530

    feat: Initial commit for demo example of dependency injetion

---
+++ BM.BookStore/Data/BookStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BM.BookStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.BookStore.Data
+{
+    public class BookStoreContext : IdentityDbContext<ApplicationUser>
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<BookGallery> BookGallery { get; set; }
+
+        public DbSet<Language> Language { get; set; }
+    }
+}
