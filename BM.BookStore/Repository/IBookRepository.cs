9d7641028700b8acff9e4e2312b02cbed70c483d
bharat.mane <bharat.mane@outlook.com>
2021-10-27 16:37:09 +0530

    feat: add an interface for repository

---
+++ BM.BookStore/Repository/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BM.BookStore.Models;
+
+namespace BM.BookStore.Repository
+{
+    public interface IBookRepository
+    {
+        Task<int> AddNewBook(BookModel model);
+        Task<List<BookModel>> GetAllBooks();
+        Task<List<BookModel>> GetTopBooksAsync(int count);
+        Task<BookModel> GetBookById(int id);
+        List<BookModel> SearchBook(string title, string authorName);
+        string GetAppName();
+    }
+}
