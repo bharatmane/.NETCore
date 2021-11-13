2a24b93ac2c3dad16519bcf0464e09b1d82d6ba9
bharat.mane <bharat.mane@outlook.com>
2021-11-13 12:46:20 +0530

    feat: demonstrated the concept for Covariance with Delegates

--- BM.CoAndContraVarianceDemo/Program.cs
+++ BM.CoAndContraVarianceDemo/Program.cs
@@ -4,7 +4,7 @@ namespace BM.CoAndContraVarianceDemo
 {
     public class Animal
     {
-        public void Eat() => Console.Write("Eat");
+        public void Eat() => Console.Write("Eat"); //Using expression body feature
     }
     public class Bird: Animal
     {
@@ -20,7 +20,21 @@ namespace BM.CoAndContraVarianceDemo
         public static Animal GetAnimal() => new Animal();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1. Covariance with Delegates : Preserves the assignment compatibility
+            Animal animal = new Bird();             //valid 
+            ReturnAnimalDelegate ad1 = GetAnimal;   //valid 
+            ReturnAnimalDelegate ad2 = GetBird;     //valid 
+            animal = ad2();                         //valid
+
+            //Bird bird = new Animal();             //invalid
+            //ReturnBirdDelegate bd1 = GetAnimal;   //invalid
+            //The reason is bd1.Eat() is fine, but bd1.Fly() won't be available
+            //as the instance is animal
+
+            
+
+
+
         }
     }
 }
