e709df168b811a58d242ac34cdef78aacc590220
bharat.mane <bharat.mane@outlook.com>
2021-11-13 12:55:51 +0530

    feat: demonstrated Contra variance with Delegates

--- BM.CoAndContraVarianceDemo/Program.cs
+++ BM.CoAndContraVarianceDemo/Program.cs
@@ -11,13 +11,24 @@ namespace BM.CoAndContraVarianceDemo
         public void Fly() => Console.Write("Fly");
     }
 
+    //For covariance with delegates
     internal delegate Animal ReturnAnimalDelegate();
     internal delegate Bird ReturnBirdDelegate();
-    
+
+    //For contra variance with delegates
+    internal delegate void TakeAnimalDelegate(Animal animal);
+    internal delegate void TakeBirdDelegate(Bird bird);
+
     class Program
     {
+        //For covariance with delegates
         public static Bird GetBird() => new Bird();
         public static Animal GetAnimal() => new Animal();
+
+        //For contra variance with delegates
+        public static void Eat(Animal animal) => animal.Eat();
+        public static void Fly(Bird bird) => bird.Fly();
+
         static void Main(string[] args)
         {
             //1. Covariance with Delegates : Preserves the assignment compatibility
@@ -31,7 +42,17 @@ namespace BM.CoAndContraVarianceDemo
             //The reason is bd1.Eat() is fine, but bd1.Fly() won't be available
             //as the instance is animal
 
-            
+            //2. Contra variance with Delegates : reverses the assignment compatibility
+            TakeBirdDelegate tbDelegate1 = Fly;      //valid
+            TakeBirdDelegate tbDelegate2 = Eat;      //valid
+
+            tbDelegate2(new Bird());                 //valid
+
+            //Below stuff won't work because every animal can eat but it can't fly
+            //TakeAnimalDelegate taDelegate1 = Fly;   //invalid
+            //taDelegate1(new Animal());              //invalid
+
+
 
 
 
