566e2ce919aa80297c575af7cfd1dbcd7bed116c
bharat.mane <bharat.mane@outlook.com>
2021-11-13 12:34:27 +0530

    feat: initial commit

---
+++ BM.CoAndContraVarianceDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BM.CoAndContraVarianceDemo
+{
+    public class Animal
+    {
+        public void Eat() => Console.Write("Eat");
+    }
+    public class Bird: Animal
+    {
+        public void Fly() => Console.Write("Fly");
+    }
+
+    internal delegate Animal ReturnAnimalDelegate();
+    internal delegate Bird ReturnBirdDelegate();
+    
+    class Program
+    {
+        public static Bird GetBird() => new Bird();
+        public static Animal GetAnimal() => new Animal();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
